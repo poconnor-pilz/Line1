85656d0a00aaec87af51349993e62472b73313fe
poconnor-pilz <p.oconnor@pilz.ie>
2026-08-10 10:10:09 +0100

    initial commit

---
+++ Device1.js
@@ -0,0 +1,17 @@
+
+
+var device1 = {
+
+    module1 : {
+    in1: 0,
+    in2: 0,
+    },
+
+    module2 : {
+    in1: 0,
+    in2: 0,
+    }
+
+}
+
+module.exports = device1;
---
+++ POU_1.js
@@ -0,0 +1,6 @@
+var POU_1 = {
+    var1: 0,
+    var2: 0,
+}
+
+module.exports = POU_1;
