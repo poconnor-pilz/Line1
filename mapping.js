5ea739ca51a5ab32656115a65116d0c091e2edd8
poconnor-pilz <p.oconnor@pilz.ie>
2026-08-13 16:14:50 +0100

    user 1 change

--- mapping.js
+++ mapping.js
@@ -2,6 +2,6 @@ const Device_0 = require('./Device_0');
 const POU_1 = require('./POU_1');
 
 POU_1.inVar = Device_0.ModuleBus.M0.I0
-POU_1.outVar = Device_0.ModuleBus.M1.O0
+POU_1.outVar = Device_0.ModuleBus.M1.O1
 POU_1.inVarUser1 = Device_0.ModuleBus.M0.I1
 
