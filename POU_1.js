c830c6bfe2c17e07fdb5ac7a9df9804dc0abfba2
poconnor-pilz <p.oconnor@pilz.ie>
2026-08-13 16:12:12 +0100

    User2 changes

--- POU_1.js
+++ POU_1.js
@@ -1,6 +1,8 @@
 var POU_1 = {
     inVar: 0,
     outVar: 0,
+    outVarUser2: 0,
+    outVarUser2Device1: 0
 }
 
 module.exports = POU_1;
--- mapping.js
+++ mapping.js
@@ -1,6 +1,9 @@
 const Device_0 = require('./Device_0');
+const Device_1 = require('./Device_1');
 const POU_1 = require('./POU_1');
 
 POU_1.inVar = Device_0.ModuleBus.M0.I0
 POU_1.outVar = Device_0.ModuleBus.M1.O0
+POU_1.outVarUser2 = Device_0.ModuleBus.M1.O0
+POU_1.outVarUser2Device1 = Device_1.ModuleBus.M1.O0
 
